09cb8af21e850043ec869136330e0c4a8dcb57ae
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-21 18:09:17 +0530

    Initial Commit

---
+++ NavDrawer/NavDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavDrawerApp.swift
+//  NavDrawer
+//
+//  Created by shehan karunarathna on 2022-02-21.
+//
+
+import SwiftUI
+
+@main
+struct NavDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
